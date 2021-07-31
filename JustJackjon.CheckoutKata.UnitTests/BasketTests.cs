21aca15f45f0746b2073406c1d742627918bea98
Jackjon Butcher <jackjon@redrobincreative.com>
2021-07-31 17:22:37 +0100

    feat(UnitTests): add test for one multiple of three promotion for item B

--- JustJackjon.CheckoutKata.UnitTests/BasketTests.cs
+++ JustJackjon.CheckoutKata.UnitTests/BasketTests.cs
@@ -60,5 +60,26 @@ namespace JustJackjon.CheckoutKata.UnitTests
             // Assert
             Assert.That(actual, Is.EqualTo(expected));
         }
+
+        [Test]
+        public void ShouldApplyPromotion3For40WhenOneMultipleOfThreeLotsOfItemBAddedToBasket()
+        {
+            // Arrange
+            const decimal expected = 40m;
+            var basket = new Basket();
+            string[] itemSkus = {"B", "B", "B"};
+
+            // Act
+            foreach (var sku in itemSkus)
+            {
+                var item = GetTestItemBySku(sku);
+                basket.AddItem(item);
+            }
+
+            var actual = basket.GetTotalCost();
+
+            // Assert
+            Assert.That(actual, Is.EqualTo(expected));
+        }
     }
 }
